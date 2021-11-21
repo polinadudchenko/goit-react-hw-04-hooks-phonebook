bb2300bd2184f41d0dd68f9bf559a4fb747e06f8
Polina Dudchenko <pv.dudchenko@gmail.com>
2021-11-21 20:38:07 +0100

    before devide the contacts state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useEffect, useState} from 'react';
 import { v1 as uuidv1 } from 'uuid';
 import s from './App.module.css';
 import { getDataFromLocalStorage } from './utils/helpers-local-storage'
@@ -8,76 +8,82 @@ import ContactList from './components/ContactList';
 import Filter from './components/Filter'
 import Section from './components/Section'
 
-class App extends Component {
-  
-  state = {
-    contacts: [],
-    filter: '',
-  }
+const useLocalStorage = (key, defaultValue) => {
+  const [state, setState] = useState(() => {
+    return getDataFromLocalStorage(key) ?? defaultValue
+  })
 
-  componentDidMount() {
+  useEffect(() => {
+    writeDataToLocalStorage(key, state)
+  }, [key, state])
+
+  return [state, setState]
+}
+
+export default function App() {
+  const [name, setName] = useLocalStorage('name', '');
+  const [name, setName] = useLocalStorage('name', '');
+  const [filter, setFilter] = useState('');
+  
+  /* const componentDidMount() {
     const parsedContacts = getDataFromLocalStorage('contacts');
     if (parsedContacts) {
       this.setState({ contacts: parsedContacts });
     }
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  const componentDidUpdate(prevProps, prevState) {
     const { contacts } = this.state;
     if (contacts !== prevState.contacts) {
       writeDataToLocalStorage('contacts', this.state.contacts);
     }
     
-  }
+  } */
 
-  onHandlerSubmit = data => {
-    if (this.state.contacts.find(contact => contact.name.toLowerCase() === data.name.toLowerCase())) {
+  const onHandlerSubmit = (name, number) => {
+    if (contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase())) {
       return alert('This contact already in the addressbook')
     }
-    this.setState((prevState) => ({
-        contacts: [...prevState.contacts, { id: uuidv1(), name: data.name, number: data.number }]
+    setContacts((contacts) => ({
+        contacts: [...contacts, { id: uuidv1(), name, number}]
         }))
   }
 
-  handleFilter = (e) => {
-    this.setState({filter: e.currentTarget.value})
+  const handleFilter = (e) => {
+    setFilter(e.currentTarget.value)
   }
 
-  filterContacts = () => {
-    const filteredContacts = this.state.contacts.filter(contact => contact.name.toLowerCase().includes(this.state.filter.toLowerCase()))
+  const filterContacts = () => {
+    const filteredContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
     return filteredContacts;
   }
 
-  deleteContact = (contactId) => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId)
+  const deleteContact = (contactId) => {
+    setContacts(contacts => ({
+      contacts: contacts.filter(contact => contact.id !== contactId)
     }))
   }
 
-  handleAlert = () => {
+  const handleAlert = () => {
     alert('No matches is found')
   }
 
-  render() {
-    return (
+  return (
       <div className={s.App}>
         <h1 className={s.App_title}>Phonebook</h1>
         <div className={s.App_content}>
         <Section title="Create a new Contact">
-        <ContactForm onSubmit={this.onHandlerSubmit} />
+        <ContactForm onSubmit={onHandlerSubmit} />
         </Section>
        
         <Section title="Your contacts">
-            <Filter onHandleFilter={this.handleFilter} />
-            {!(this.filterContacts().length === 0)
-            && <ContactList contacts={this.state.filter ? this.filterContacts() : this.state.contacts} onDeleteContact={ this.deleteContact}/>
+            <Filter onHandleFilter={handleFilter} />
+            {!(filterContacts().length === 0)
+            && <ContactList contacts={filter ? filterContacts() : contacts} onDeleteContact={ deleteContact}/>
             }
           </Section>
         </div>
     </div>
   )
-  }
-  
 }
 
-export default App;
