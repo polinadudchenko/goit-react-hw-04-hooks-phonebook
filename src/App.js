837766960489f6420429d6b48570cf64ce3d272a
Polina Dudchenko <pv.dudchenko@gmail.com>
2021-11-21 23:50:20 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import { useState} from 'react';
+import { useEffect, useState } from 'react';
 import { v1 as uuidv1 } from 'uuid';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import s from './App.module.css';
 import { useLocalStorage } from './utils/hooks/useLocalStorage';
 import ContactForm from './components/ContactForm';
@@ -7,13 +9,15 @@ import ContactList from './components/ContactList';
 import Filter from './components/Filter'
 import Section from './components/Section'
 
+var debounce = require('lodash.debounce');
+
 export default function App() {
   const [contacts, setContacts] = useLocalStorage('contacts', {});
   const [filter, setFilter] = useState('');
 
   const onHandlerSubmit = (name, number) => {
     if (contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase())) {
-      return alert('This contact already in the addressbook')
+      return toast.info('This contact already in the addressbook')
     }
     setContacts((contacts) => ([...contacts, { id: uuidv1(), name, number}]))
   }
@@ -21,6 +25,9 @@ export default function App() {
   const handleFilter = (e) => {
     setFilter(e.currentTarget.value)
   }
+
+  useEffect(() => { filterContacts().length === 0 && toast.error('No matches are found') }, [filter])
+  
 
   const filterContacts = () => {
     const filteredContacts = contacts.filter(contact => (contact.name.toLowerCase().includes(filter.toLowerCase())))
@@ -31,10 +38,6 @@ export default function App() {
     setContacts(contacts => (contacts.filter(contact => contact.id !== contactId)))
   }
 
-  /* const handleAlert = () => {
-    alert('No matches is found')
-  } */
-
   return (
       <div className={s.App}>
         <h1 className={s.App_title}>Phonebook</h1>
@@ -44,16 +47,19 @@ export default function App() {
         </Section>
        
         <Section title="Your contacts">
-          {!(filterContacts().length === 0)
-            ?
+          {contacts.length === 0 ?
+            <p>You don't have any contacts yet, please add one to the form on the left</p>
+            :
             <>
             <Filter onHandleFilter={handleFilter} />
-            <ContactList contacts={filter ? filterContacts() : contacts} onDeleteContact={deleteContact} />
-            </>
-            :
-            <p>You don't have any contacts yet, please add one to the form on the left</p>
+            {!(filterContacts().length === 0)
+              &&
+              <ContactList contacts={filter ? filterContacts() : contacts} onDeleteContact={deleteContact} /> 
             }
-          </Section>
+            </>
+          }
+        </Section>
+        <ToastContainer />
         </div>
     </div>
   )
