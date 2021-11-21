fcff8f95332802b6383c062eb0a39b4e17bdaa49
Polina Dudchenko <pv.dudchenko@gmail.com>
2021-11-21 16:35:39 +0100

    hooks for contact form done

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,41 +1,44 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types'
 import s from './ContactForm.module.css'
 
-const INITIAL_STATE = {
-    name: '',
-    number: '',
-};
-
-class ContactForm extends Component{
-    static propTypes = {
-        onSubmit: PropTypes.func.isRequired,
-    }
-    state = {...INITIAL_STATE}
+export default function ContactForm({onSubmit}){
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
     
-    handleChange = (e) => {
-        this.setState({ [e.currentTarget.name]: e.target.value })
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        switch (name) {
+            case ('name'):
+                setName(value);
+                break;
+            case ('number'):
+                setNumber(value);
+                break;
+            default:
+                throw new Error();
+        }
     }
   
-    handleSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-        this.props.onSubmit({ ...this.state });
-        this.reset();
+        onSubmit(name, number);
+        reset();
     }
     
-    reset = () => {
-        this.setState({...INITIAL_STATE})
+    const reset = () => {
+        setName('');
+        setNumber('');
     }
-    render() {
-        const { name, number } = this.state;
-        return <form className={s.contact_form} onSubmit={this.handleSubmit}>
+
+    return (<form className={s.contact_form} onSubmit={handleSubmit}>
             <label className={s.contact_form__label}> Name
                 <input
                     type="text"                
                     className={s.contact_form__input}
                     name="name"
                     value={name}
-                    onChange={this.handleChange}
+                    onChange={handleChange}
                     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                     title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
                     required
@@ -47,7 +50,7 @@ class ContactForm extends Component{
                     name="number"
                     value={number}
                     className={s.contact_form__input}
-                    onChange={this.handleChange}
+                    onChange={handleChange}
                     pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                     title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
                     required
@@ -56,10 +59,9 @@ class ContactForm extends Component{
             <button type="submit" className={s.contact_form__button} >
             Add to contacts
             </button>
-        </form> 
-    }
-    
-
+        </form> )
 }
 
-export default  ContactForm
+ContactForm.propTypes = {
+        onSubmit: PropTypes.func.isRequired,
+}
